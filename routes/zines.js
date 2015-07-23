c5ebb0e6f45c834dd64852d2fead90208f0d8515
Anna Casper <anna.casper3@gmail.com>
2015-07-23 13:34:49 -0600

    routes are complete

--- routes/zines.js
+++ routes/zines.js
@@ -28,5 +28,36 @@ router.post('/articles', function(req, res, next){
   res.redirect('/zines')
 });
 
+//GET show page
+router.get('/articles/:id', function(req, res, next){
+  zineCollection.findOne({_id: req.params.id}, function(err, thisZine){
+    res.render('zines/show', {thisZine: thisZine});
+  });
+});
+
+//GET edit page
+router.get('/articles/:id/edit', function(req, res, next){
+  zineCollection.findOne({_id: req.params.id}, function(err, thisZine){
+    res.render('zines/edit', {thisZine: thisZine});
+  });
+});
+
+//POST updates
+router.post('/articles/:id/update', function(req, res, next){
+  zineCollection.update({_id: req.params.id}, {$set: {
+    title: req.body.title,
+    backgroundUrl: req.body.backgroundUrl,
+    darkBackground: req.body.darkBackground,
+    excerpt: req.body.excerpt,
+    body: req.body.body
+  }});
+  res.redirect('/zines/articles/' + req.params.id)
+});
+
+//POST remove
+router.get('/delete/:id', function(req, res, next){
+  zineCollection.remove({_id: req.params.id});
+  res.redirect('/zines/articles')
+})
 
 module.exports = router;
